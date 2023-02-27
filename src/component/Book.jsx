e4103e104444d7f52cae33f636ff286cc4486146
Eric <eric2umeh@yahoo.com>
2023-02-27 19:11:48 +0100

    Create BookList.jsx

--- src/component/Book.jsx
+++ src/component/Book.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
+import { removeBook } from '../redux/books/booksSlice';
 
-const Book = ({ title, author }) => (
-  <div>
-    <ul>
-      <li>{title}</li>
-      <li>{author}</li>
-      <button type="submit">Remove</button>
-    </ul>
-  </div>
-);
+const Book = ({ id, author, title }) => {
+  const dispatch = useDispatch();
+
+  const handleRemove = () => {
+    dispatch(removeBook(id));
+  };
+
+  return (
+    <div>
+      <div>
+        <h3>{title}</h3>
+        <p>{author}</p>
+        <div>
+          <button type="submit" onClick={handleRemove}>
+            Remove
+          </button>
+          <button type="submit">Edit</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Book;
