c1dabe54295697c5c13df9a1ef0d088cf8961b74
Eric <eric2umeh@yahoo.com>
2023-02-28 19:59:40 +0100

    Update Categories.jsx Button

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,26 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Button from '../component/Button';
-import { checkStatus } from '../redux/categories/categoriesSlice';
+import { checkStatus, bookOrigin } from '../redux/categories/categoriesSlice';
 
 const Categories = () => {
   const { categories, message } = useSelector((state) => state.categories);
-  const [isChecking, setIsChecking] = useState(false);
   const dispatch = useDispatch();
 
   const handleCheckStatus = () => {
-    setIsChecking(true);
     dispatch(checkStatus());
   };
 
+  const handleCountry = () => {
+    dispatch(bookOrigin());
+  };
+
   return (
-    <div>
-      <Button type="submit" handleClick={handleCheckStatus}>
-        {isChecking ? 'Checking..' : 'Check Status'}
-      </Button>
-      <div>{categories}</div>
-      <div>{message}</div>
-    </div>
+    <>
+      <main>
+        <Button type="submit" handleClick={handleCheckStatus}>
+          Check Status
+        </Button>
+        <Button type="submit" handleClick={handleCountry}>
+          Check Country
+        </Button>
+        <div>{categories}</div>
+        <div>{message}</div>
+      </main>
+    </>
   );
 };
 
