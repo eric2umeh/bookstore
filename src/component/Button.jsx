999bb0dc1c8f62d1bf656d6d791a2df010e8309b
Eric <eric2umeh@yahoo.com>
2023-02-28 19:34:23 +0100

    Fixed Button Component

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ type, value, handleClick }) => (
-  <input type={type ?? 'button'} value={value} onClick={handleClick} />
+const Button = ({
+  type, value, handleClick, children,
+}) => (
+  <button type={type ? 'submit' : 'button'} value={value} onClick={handleClick}>{children}</button>
 );
 
 Button.propTypes = {
   type: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   handleClick: PropTypes.func.isRequired,
 };
