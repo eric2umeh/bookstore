ee36eaaff8fca0a825db3ddd8b87d068337a7886
Eric <eric2umeh@yahoo.com>
2023-03-02 13:08:18 +0100

    Update api unique ID key using curl -X POST endpoint

--- src/url.js
+++ src/url.js
@@ -1,3 +1,3 @@
-const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/XMpcykCejhyFfnA1ddnb';
+const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/HFKKkJyJDmrbQjhyDqlG';
 
 export default URL;
