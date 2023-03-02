6a52ae86cf817f3b419fcd231737cdbec1fdfdae
Eric <eric2umeh@yahoo.com>
2023-03-02 11:26:47 +0100

    Add post action and select functionality to Form.js

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,24 +1,33 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { v4 } from 'uuid';
-import { addBook } from '../redux/books/booksSlice';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook, postBook } from '../redux/books/booksSlice';
 
 const Form = () => {
   const [author, setAuthor] = useState('');
   const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+
   const dispatch = useDispatch();
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (!title.trim() || !author.trim()) return;
+
     const newBook = {
-      id: v4(),
+      item_id: uuidv4(),
       title,
       author,
+      category: 'Fiction',
     };
 
+    // Add book to state
     dispatch(addBook(newBook));
-    setTitle('');
+    dispatch(postBook(newBook));
+
+    // Empty form inputs
     setAuthor('');
+    setTitle('');
   };
 
   return (
@@ -28,6 +37,7 @@ const Form = () => {
           type="text"
           placeholder="Book's Title"
           id="title"
+          aria-label="Book title input...."
           name="title"
           onChange={(event) => setTitle(event.target.value)}
           value={title}
@@ -37,9 +47,18 @@ const Form = () => {
           placeholder="Author's Name"
           name="author"
           id="author"
+          aria-label="Book author input"
           onChange={(event) => setAuthor(event.target.value)}
           value={author}
         />
+        <select name="category" id="category" onChange={(event) => setCategory(event.target.value)} aria-label="Book Category" required>
+          <option value="">Choose Category...</option>
+          <option value="Technology">Technology</option>
+          <option value="Economics">Economics</option>
+          <option value="Language">Language</option>
+          <option value="Science">Science</option>
+          <option value="Politics">Politics</option>
+        </select>
         <button type="submit">Add Book</button>
       </form>
     </div>
