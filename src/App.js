f0b72a1ea763aa50a533cd161b3df663627df31d
Eric <eric2umeh@yahoo.com>
2023-03-02 17:02:27 +0100

    Update the App.js with Layout.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navigation from './component/Navigation';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import Books from './pages/Books';
 import Categories from './pages/Categories';
+import Layout from './layout/Layout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Books />} />
+      <Route path="categories" element={<Categories />} />
+    </Route>,
+  ),
+);
 
 function App() {
-  return (
-    <>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
